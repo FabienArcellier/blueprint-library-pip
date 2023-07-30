ef5d3e9a87a7b4e59bd27614cbb554847aedca8a
Fabien Arcellier <fabien.arcellier@gmail.com>
2023-07-30 16:09:45 +0200

    implement sphinx documentation

---
+++ docs/index.rst
@@ -0,0 +1,8 @@
+Blueprint-library-pip documentation
+===================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   api
--- src/lib/main.py
+++ src/lib/main.py
@@ -1,4 +1,9 @@
 def hello_world():
+    """
+    Display hello world on the console
+
+    >>> hello_world()
+    """
     print("hello world")
 
 if __name__ == "__main__":
