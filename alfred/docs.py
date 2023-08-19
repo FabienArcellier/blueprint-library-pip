88ccd76e4978336345356f19162e10649ef7ed1f
Fabien Arcellier <fabien.arcellier@gmail.com>
2023-08-19 08:11:25 +0200

    add docs:display command to display documentation in browser

--- alfred/docs.py
+++ alfred/docs.py
@@ -1,11 +1,26 @@
 import os
 
 import alfred
+import click
 
 
-@alfred.command('docs:html', help="build documentation in html format")
+@alfred.command('docs:html', help="build documentation with sphinx")
 def docs_html():
     make = alfred.sh('make')
     directory = alfred.project_directory()
     os.chdir(os.path.join(directory, 'docs'))
     alfred.run(make, 'html')
+
+
+@alfred.command('docs:display', help="open documentation in the browser")
+def docs_display():
+    open_browser = alfred.sh(["open", "xdg-open"])
+    directory = alfred.project_directory()
+
+    documentation_directory = os.path.join(directory, 'docs', '_build', 'html')
+    if os.path.isdir(documentation_directory) == False:
+        alfred.invoke_command('docs:html')
+
+    os.chdir(os.path.join(directory, 'docs', '_build', 'html'))
+    click.echo(f"opening {documentation_directory}/index.html")
+    alfred.run(open_browser, 'index.html')
